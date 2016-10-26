81b9736da631374f05e3b734930ee195a30c00ca
Per Arne <per.arne.kjelsvik@gmail.com>
2016-10-26 17:33:54 +0200

    Moved into a folder
    Github should improve folder structuring

--- Helicopter-Lab/init_heli.m
+++ Helicopter-Lab/init_heli.m
@@ -3,9 +3,9 @@
 % the course TTK4115. Run this file before you execute QuaRC_ -> Build 
 % to build the file heli_q8.mdl.
 
-% Oppdatert høsten 2006 av Jostein Bakkeheim
-% Oppdatert høsten 2008 av Arnfinn Aas Eielsen
-% Oppdatert høsten 2009 av Jonathan Ronen
+% Oppdatert hÃ¸sten 2006 av Jostein Bakkeheim
+% Oppdatert hÃ¸sten 2008 av Arnfinn Aas Eielsen
+% Oppdatert hÃ¸sten 2009 av Jonathan Ronen
 % Updated fall 2010, Dominik Breu
 % Updated fall 2013, Mark Haring
 % Updated spring 2015, Mark Haring
