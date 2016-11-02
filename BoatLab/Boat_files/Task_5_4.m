a3ed89fd50d9fbea6a4f3f28ed02f76528446be7
PerKjelsvik <per.arne.kjelsvik@gmail.com>
2016-11-02 21:21:23 +0100

    Some cleanup, removed unwanted html-files

--- BoatLab/Boat_files/Task_5_4.m
+++ BoatLab/Boat_files/Task_5_4.m
@@ -18,30 +18,46 @@ E_bw = [0 0; K_w 0; 0 0; 0 0; 0 1];
 %
 
 %% TASK 5.4.b ---- Observabillity without disturbances
-A =
-C = 
+A = [0 1; 0 -1/T];
+B=[0; K/T];
+C= [1 0];
+O = obsv(A,C);
 
-% If C_obs = , we have full rank <=> Observabillity
-C_obs = rank(obsv(A,C));
+% If rank(O) = 2, we have full rank <=> Observabillity
+rank_O = rank(O);
 %
 
 %% TASK 5.4.c ---- Observabillity with current
-A_b =  
-C_b =
+A_b = [0 1 0; 0 -1/T -K/T; 0 0 0]; 
+C_b = [1 0 0];
+O_b = obsv(A_b,C_b);
 
-% If C_obs_b = 5, we have full rank <=> Observabillity
-C_obs_b = rank(obsc(A_b,C_b));
+% If rank(O_b) = 3, we have full rank <=> Observabillity
+rank_O_b = rank(O_b);
 %
 
 %% TASK 5.4.d ---- Observabillity with wave
-A_w = 
-C_w = 
+A_w = [0 1 0 0; -omega_0^2 -2*lambda*omega_0 0 0; 0 0 0 1; 0 0 0 -1/T];
+C_w = [0 1 1 0];
+O_w = obsv(A_w,C_w);
 
-% If C_obs_w = 5, we have full rank <=> Observabillity
-C_obs_w = rank(obsc(A_w,C_w));
+% If rank(O_w) = 4, we have full rank <=> Observabillity
+rank_O_w = rank(O_w);
 %
 
 %% TASK 5.4.e ---- Observabillity with current and wave
-% If C_obs_bw = 5, we have full rank <=> Observabillity
-C_obs_bw = rank(obsv(A_w,C_w));
+O_bw = obsv(A_bw,C_bw);
+
+% If rank(O_bw) = 5, we have full rank <=> Observabillity
+rank_O_bw = rank(O_bw); 
+%
+
+%% ------- PRINT RESULTS ---------
+fprintf('\n\n\t\t*----------------------------------------------------------*\n');
+fprintf('\t\t| \t  TABLE\t\t| ideal\t|current|  wave\t| current and wave |\n');
+fprintf('\t\t*----------------------------------------------------------*\n');
+fprintf('\t\t| required rank\t|\t2\t|\t3\t|\t4\t|\t\t  5\t\t   |\n');
+fprintf('\t\t|  actual rank\t|\t%i\t|\t%i\t|\t%i\t|\t\t  %i\t\t   |\n', rank_O, rank_O_b, rank_O_w, rank_O_bw);
+fprintf('\t\t|  Observable?\t|  Yes\t|  Yes\t|  Yes\t|\t\t Yes\t   |\n');
+fprintf('\t\t*----------------------------------------------------------*\n\n');
 %
