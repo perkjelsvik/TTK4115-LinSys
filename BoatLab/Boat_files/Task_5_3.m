b7f43a4f4cdd04a6b3675defb94a7d67ea3f50e1
PerKjelsvik <per.arne.kjelsvik@gmail.com>
2016-11-02 17:28:18 +0100

    5.1 - 5.4 complete! Kalman remains
    Have completed large parts of the lab. Discrete Kalman filter remains.
    For some reason, my commit 1 week ago didn't get synced.

---
+++ BoatLab/Boat_files/Task_5_3.m
@@ -0,0 +1,68 @@
+%% Init
+close all
+clc
+clear all
+
+figNum = 1;
+addpath('Simulink models tasks')
+%
+
+%% TASK 5.3.a ---- Designing a PD-controller
+s = tf('s');
+K = 0.1742;
+T = 86.5256;
+T_d = T;
+w_c = 0.1;
+
+T_f = -1/(tan(130*pi/180)*w_c);
+K_pd = sqrt(w_c^2+T_f^2*w_c^4)/K;
+H_0 = (K*K_pd)/(s*(1+T_f*s));
+
+figure(figNum)
+figNum = figNum + 1;
+margin(H_0)
+grid on
+%
+
+%% TASK 5.3.b ---- Simulating without disturbances
+PSI_r = 30;
+sim_t = 500;
+load_system('task5_3_b.slx')
+sim('task5_3_b.slx')
+
+figure(figNum)
+figNum = figNum+1;
+plot(t,sim_PSI_r,t,sim_compass, 'LineWidth',3);
+title('Autopilot without current and waves', 'FontSize', 18);
+xlabel('t', 'FontSize', 24); grid on;
+legend({'$$\psi_{r}(t)$$', '$$\psi(t)$$'}, 'Location', ... 
+    'east', 'FontSize', 36, 'Interpreter', 'latex')
+%
+
+%% TASK 5.3.c --- Simulating with current
+load_system('task5_3_c.slx')
+sim('task5_3_c.slx')
+
+figure(figNum)
+figNum = figNum + 1;
+plot(t,sim_PSI_r,t,sim_compass, 'LineWidth',3);
+axis([0 500 0 35]);grid on
+xlabel('t', 'FontSize', 24); 
+title('Autopilot with current, but without waves', 'FontSize', 18);
+legend({'$$\psi_{r}(t)$$', '$$\psi(t)$$'}, 'Location', ... 
+    'east', 'FontSize', 36, 'Interpreter', 'latex')
+%
+
+%% TASK 5.3.d --- Simulating with current and waves
+load_system('task5_3_d.slx')
+sim('task5_3_d.slx')
+
+figure(figNum)
+figNum = figNum + 1;
+plot(t,sim_PSI_r,t,sim_compass, 'LineWidth',3);
+axis([0 500 0 35]);grid on
+xlabel('t', 'FontSize', 24); 
+title('Autopilot with current and waves', 'FontSize', 18);
+legend({'$$\psi_{r}(t)$$', '$$\psi_{w}(t)$$'}, 'Location', ... 
+    'east', 'FontSize', 36, 'Interpreter', 'latex')
+%
---
+++ BoatLab/Boat_files/Task_5_5.m
@@ -0,0 +1,10 @@
+%% Init
+close all
+clc
+clear all
+
+addpath('Data-files')
+% Constants from previous tasks witout disturbances
+load('constants_5.2.mat')
+load('constants_5.3.mat')
+%
